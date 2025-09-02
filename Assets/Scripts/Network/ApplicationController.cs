a93a7726febf3e8625fb64a74b85b5ce4c29c86a
EdgarNovas <edgarhipernovas@gmail.com>
2025-09-02 02:40:19 +0200

    auth

--- Assets/Scripts/Network/ApplicationController.cs
+++ Assets/Scripts/Network/ApplicationController.cs
@@ -25,10 +25,15 @@ public class ApplicationController : MonoBehaviour
         {
             //Not a dedicated server usually this
             ClientSingleton clientSingleton = Instantiate(clientPrefab);
-            await clientSingleton.CreateClient();
+            bool authenticated = await clientSingleton.CreateClient();
 
             HostSingleton hostSingleton = Instantiate(hostPrefab);
             hostSingleton.CreateHost();
+
+            if(authenticated)
+            {
+                clientSingleton.GameManager.GoToMenu();
+            }
         }
     }
 
